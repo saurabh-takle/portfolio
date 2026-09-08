7fb16ab9da6503f3d77774e476d3f27b7e61cf6d
Saurabh Takle <saurabh.takle@gmail.com>
2026-09-08 09:40:11 +0530

    Update resume download link to new file name

--- front-end/src/app/components/DownloadResume.tsx
+++ front-end/src/app/components/DownloadResume.tsx
@@ -15,7 +15,7 @@ const DownloadResume: React.FC = () => {
             className="fixed bottom-4 right-4"
         >
             <motion.a 
-                href="/SaurabhTakle.pdf" 
+                href="/SaurabhTakleResume.pdf" 
                 download="Saurabh_Takle_Resume.pdf"
                 className="pointer-events-auto bg-[url('/brush.svg')] text-[#171717] py-2 px-4 rounded-full flex items-center"
                 whileHover={{ scale: 1.15 }}
